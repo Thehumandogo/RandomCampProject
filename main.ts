61a4c739e38928a389b27ba856297de4b0b6c96c
Thehumandogo <corbinrieske@gmail.com>
2025-06-12 09:09:14 -0500

    Dont wanna have to restart

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
+function ShowHappyPet () {
+    basic.showIcon(IconNames.Happy)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+}
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(1000)
     petHunger = 0
     petHappiness = Math.min(10, petHappiness + 2)
+    ShowHappyPet()
     basic.pause(500)
 })
 let petHunger = 0
 let petHappiness = 0
 petHappiness = 5
 let petEnergy = 5
-petHunger = 0
-let lastPlayTime = 0
-let lastFeedTime = 0
+ShowHappyPet()
 basic.forever(function () {
 	
 })
